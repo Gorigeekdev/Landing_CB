7344f2d3e7c1da04b8212fd2c85a43851ce8e03d
bauDevGorigeek <alfredo.bautista@gorigeek.com>
2025-07-23 17:36:20 -0600

    Actualización - 0.0.4 - Alfredo Bautista - Correcciones para permitir la conexión al webadmin.

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { fetchCB } from '../../helpers/global.helpers';
 
 export type HomeScreenProps = {
 	// types...
 }
 
 const HomeScreen: React.FC<HomeScreenProps>  = ({}) => {
+
+	// async function handleClick() {
+	// 	let data = {
+	// 		'data1' : 'colochotester123@gmail.com',
+	// 		'data2' : 'romeo123',
+	// 		'data3' : false
+	// 	};
+
+	// 	const onCliked = await fetchCB('session', data, 'POST');
+
+	// 	console.log(onCliked);
+	// }
+
 	return (
 		<div>
  			HomeScreen works!
